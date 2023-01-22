07cf495db40eb25954676cfdbd5317f54ff13116
Hardik Garg <55681128+kone807@users.noreply.github.com>
2023-01-22 12:57:01 +0530

    removed unused portions

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ def run_model(x_train, x_test, y_train, y_test, custom):
     model.fit(x_train,y_train)
     y_pred = model.predict(x_test)
     print(25*"##")
-    print("LOGISTIC REGRESSION")
+    print(args.model)
     print("Testing Data:")
     print("Accuracy:",accuracy_score(y_test,y_pred))
     print("F1 Score:",f1_score(y_test,y_pred, average="weighted"))
@@ -87,159 +87,8 @@ def run_model(x_train, x_test, y_train, y_test, custom):
     print("Testing Data:")
     print("Accuracy:",accuracy_score(y_test,preds))
     print("F1 Score:",f1_score(y_test,preds, average="weighted"))
-#     ## logistic regression
-#     model_logistic = LogisticRegression()
-#     model_logistic.fit(x_train,y_train)
-#     y_pred = model_logistic.predict(x_test)
-#     print(25*"##")
-#     print("LOGISTIC REGRESSION")
-#     print("Testing Data:")
-#     print("Accuracy:",accuracy_score(y_test,y_pred))
-#     print("F1 Score:",f1_score(y_test,y_pred, average="weighted"))
-#     #print(classification_report(y_test,y_pred))
-#     #print(25*"**")
-#     y_pred = model_logistic.predict(x_train)
-#     print(25*"--")
-#     print("Training Data:")
-#     print("Accuracy:",accuracy_score(y_train,y_pred))
-#     print("F1 Score:",f1_score(y_train,y_pred, average="weighted"))
-#     ##print(classification_report(y_train,y_pred))
     
-#     ## SGD classifier
-#     model_SGD = SGDClassifier()
-#     model_SGD.fit(x_train,y_train)
-#     y_pred = model_SGD.predict(x_test)
-#     print(25*"##")
-#     print("SGD CLASSIFIER")
-#     print("Testing Data:")
-#     print("Accuracy:",accuracy_score(y_test,y_pred))
-#     print("F1 Score:",f1_score(y_test,y_pred, average="weighted"))
-#     #print(classification_report(y_test,y_pred))
-#     #print(25*"**")
-#     y_pred = model_SGD.predict(x_train)
-#     print(25*"--")
-#     print("Training Data:")
-#     print("Accuracy:",accuracy_score(y_train,y_pred))
-#     print("F1 Score:",f1_score(y_train,y_pred, average="weighted"))
-#     ##print(classification_report(y_train,y_pred))
-    
-#     ## Naive Bayes
-#     model_NB = GaussianNB()
-#     model_NB.fit(x_train,y_train)
-#     y_pred = model_NB.predict(x_test)
-#     print(25*"##")
-#     print("GAUSSIAN NAIVE BAYES")
-#     print("Testing Data:")
-#     print("Accuracy:",accuracy_score(y_test,y_pred))
-#     print("F1 Score:",f1_score(y_test,y_pred, average="weighted"))
-#     #print(classification_report(y_test,y_pred))
-#     #print(25*"**")
-#     y_pred = model_NB.predict(x_train)
-#     print(25*"--")
-#     print("Training Data:")
-#     print("Accuracy:",accuracy_score(y_train,y_pred))
-#     print("F1 Score:",f1_score(y_train,y_pred, average="weighted"))
-#     ##print(classification_report(y_train,y_pred))
-    
-#     ## Random Forest
-#     model_RF = RandomForestClassifier()
-#     model_RF.fit(x_train,y_train)
-#     y_pred = model_RF.predict(x_test)
-#     print(25*"##")
-#     print("RANDOM FOREST")
-#     print("Testing Data:")
-#     print("Accuracy:",accuracy_score(y_test,y_pred))
-#     print("F1 Score:",f1_score(y_test,y_pred, average="weighted"))
-#     #print(classification_report(y_test,y_pred))
-#     #print(25*"**")
-#     y_pred = model_RF.predict(x_train)
-#     print(25*"--")
-#     print("Training Data:")
-#     print("Accuracy:",accuracy_score(y_train,y_pred))
-#     print("F1 Score:",f1_score(y_train,y_pred, average="weighted"))
-#     ##print(classification_report(y_train,y_pred))
-    
-#     ## XGBoost
-#     model_xgboost = XGBClassifier()
-#     model_xgboost.fit(x_train,y_train)
-#     y_pred = model_xgboost.predict(x_test)
-#     print(25*"##")
-#     print("XGBOOST")
-#     print("Testing Data:")
-#     print("Accuracy:",accuracy_score(y_test,y_pred))
-#     print("F1 Score:",f1_score(y_test,y_pred, average="weighted"))
-#     #print(classification_report(y_test,y_pred))
-#     #print(25*"**")
-#     y_pred = model_xgboost.predict(x_train)
-#     print(25*"--")
-#     print("Training Data:")
-#     print("Accuracy:",accuracy_score(y_train,y_pred))
-#     print("F1 Score:",f1_score(y_train,y_pred, average="weighted"))
-#     ##print(classification_report(y_train,y_pred))
-    
-#     ## AdaBoost
-#     model_adaboost = AdaBoostClassifier()
-#     model_adaboost.fit(x_train,y_train)
-#     y_pred = model_adaboost.predict(x_test)
-#     print(25*"##")
-#     print("ADABOOST")
-#     print("Testing Data:")
-#     print("Accuracy:",accuracy_score(y_test,y_pred))
-#     print("F1 Score:",f1_score(y_test,y_pred, average="weighted"))
-#     #print(classification_report(y_test,y_pred))
-#     #print(25*"**")
-#     y_pred = model_adaboost.predict(x_train)
-#     print(25*"--")
-#     print("Training Data:")
-#     print("Accuracy:",accuracy_score(y_train,y_pred))
-#     print("F1 Score:",f1_score(y_train,y_pred, average="weighted"))
-#     ##print(classification_report(y_train,y_pred))
-    
-#     ## KNN
-#     model_knn = KNeighborsClassifier()
-#     model_knn.fit(x_train,y_train)
-#     y_pred = model_knn.predict(x_test)
-#     print(25*"##")
-#     print("KNN CLASSIFIER")
-#     print("Testing Data:")
-#     print("Accuracy:",accuracy_score(y_test,y_pred))
-#     print("F1 Score:",f1_score(y_test,y_pred, average="weighted"))
-#     #print(classification_report(y_test,y_pred))
-#     #print(25*"**")
-#     y_pred = model_knn.predict(x_train)
-#     print(25*"--")
-#     print("Training Data:")
-#     print("Accuracy:",accuracy_score(y_train,y_pred))
-#     print("F1 Score:",f1_score(y_train,y_pred, average="weighted"))
-#     ##print(classification_report(y_train,y_pred))
-    
-#     ## SVM
-#     model_SVM = SVC()
-#     model_SVM.fit(x_train,y_train)
-#     y_pred = model_SVM.predict(x_test)
-#     print(25*"##")
-#     print("SVM")
-#     print("Testing Data:")
-#     print("Accuracy:",accuracy_score(y_test,y_pred))
-#     print("F1 Score:",f1_score(y_test,y_pred, average="weighted"))
-#     #print(classification_report(y_test,y_pred))
-#     #print(25*"**")
-#     y_pred = model_SVM.predict(x_train)
-#     print(25*"--")
-#     print("Training Data:")
-#     print("Accuracy:",accuracy_score(y_train,y_pred))
-#     print("F1 Score:",f1_score(y_train,y_pred, average="weighted"))
-#     ##print(classification_report(y_train,y_pred))
-
     model_dict[args.model] = model
-    # model_dict["SGD"] = model_SGD
-    # model_dict["RF"] = model_RF
-    # model_dict["KNN"] = model_knn
-    # model_dict["SVM"] = model_SVM
-    # model_dict["XGB"] = model_xgboost
-    # model_dict["ADA"] = model_adaboost
-    # model_dict["NB"] = model_NB
-    
     name = "raw_feature_model_dict"
     if custom:
         name = "custom_feature_model_dict.pkl"
@@ -259,7 +108,6 @@ def get_designed_data_results(df):
     ## dropping Pose column
     df.drop(columns=["Pose","ImgNum"],inplace=True)
     
-   
     ## feature designing for train and test
     df = get_designed_data_df(df)
     
@@ -268,20 +116,13 @@ def get_designed_data_results(df):
     
     x_train, x_test, y_train, y_test = train_test_split(x,y,stratify=y,test_size=0.25, random_state=0)
     
-    
-    
     model_dict = None
     ## run models
     model_dict = run_model(x_train, x_test, y_train, y_test,True)
     
-    ## cosine similarity
-    #get_cosine_similarity_results(x_train, x_test, y_train, y_test, df.columns)
-    
     return model_dict
 
 def main():
-    
-    #1. one-hot encode pose
     
     df = pd.read_csv("trainSet_yoga82.csv")
     # model_dict = get_raw_data_results(df)
